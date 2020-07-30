9b0687aaf4fe210223e33f8871090b0f8c28d77a
bbjorn <bbjorn@student.chalmers.se>
2020-07-30 18:19:42 +0200

    Updates

---
+++ precache-manifest.31e62a7905f2a0f5cce746387e3b9e73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ae649fe9ab39b9535c32b34f6616cc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c36bfea8c24c318873b2",
+    "url": "/portfolio/static/css/main.ab1ee05f.chunk.css"
+  },
+  {
+    "revision": "a57660f140116e6e25f5",
+    "url": "/portfolio/static/js/2.5f3b5664.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.5f3b5664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36bfea8c24c318873b2",
+    "url": "/portfolio/static/js/main.67f449b6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f6fc7936423558884e3d740c1f2480cb",
+    "url": "/portfolio/static/media/BgDusk.f6fc7936.jpg"
+  },
+  {
+    "revision": "816d6ca6be01a80dde0ba44e0d75d73f",
+    "url": "/portfolio/static/media/me.816d6ca6.jpg"
+  },
+  {
+    "revision": "9d603c17da206e9b8c57afcf4ff721f6",
+    "url": "/portfolio/static/media/mesquare.9d603c17.jpg"
+  }
+]);
