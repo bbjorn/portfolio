301d35b49c58d0069de0feea195c18d482884db8
bbjorn <bbjorn@student.chalmers.se>
2020-08-04 17:10:03 +0200

    Implemented the Design part of the portfolio

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -1,9 +1,126 @@
 import React from "react";
+import { getDesignPortfolio } from "./../services/fakeDesignService";
+import "./../css/design.css";
 
 function Design(props) {
+  const designPortfolio = getDesignPortfolio();
+  return (
+    <div>{designPortfolio.map((value) => createDesignElement(value))}</div>
+  );
+}
+
+function createDesignElement(designElement) {
   return (
     <div className="mainstyle-inner">
-      <h1> Design Portfolio</h1>
+      <h2 className="h2-box"> {designElement.title} </h2>
+      <div className="design-side-grid-container">
+        <div className="design-side-left">
+          <p> Date - {designElement.date}</p>
+          <p> Problem - {designElement.problem}</p>
+          <p> Design - {designElement.design}</p>
+          <p> Role - {designElement.role}</p>
+          <p>
+            Responsibilities:
+            <ol>
+              {designElement.responsibilities.map((value) => (
+                <li>{value}</li>
+              ))}
+            </ol>
+          </p>
+        </div>
+        <div className="design-side-right">
+          <img
+            src={designElement.image}
+            alt={designElement.imageAlt}
+            className="design-side-img"
+          />
+          {designElement.imagecredit && (
+            <p className="design-credit"> {designElement.imagecredit}</p>
+          )}
+        </div>
+      </div>
+      {designElement.entires.map((value) =>
+        createDesignEntry(value, designElement.title)
+      )}
+    </div>
+  );
+}
+
+function createDesignEntry(designEntry, title) {
+  return (
+    <React.Fragment>
+      <h2 className="h2-box"> {title + " - " + designEntry.title} </h2>
+      {designEntry.layout === "double" && createDesignEntryDouble(designEntry)}
+      {designEntry.layout === "top" && createDesignEntryTop(designEntry)}
+      {designEntry.layout === "side" && createDesignEntrySide(designEntry)}
+    </React.Fragment>
+  );
+}
+
+function createDesignEntryDouble(designEntry) {
+  return (
+    <div className="design-double-grid-container">
+      {designEntry.parts.map((value) => (
+        <React.Fragment>
+          <div style={{ order: -1 }}>
+            <img
+              src={value.image}
+              alt={value.imageAlt}
+              className="design-double-img"
+            />
+          </div>
+          <div>
+            {value.text.map((textvalue) => (
+              <p>{textvalue}</p>
+            ))}
+            {value.link && <a href={value.link}>{value.link}</a>}
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+function createDesignEntryTop(designEntry) {
+  return (
+    <div className="design-top-grid-container">
+      {designEntry.parts.map((value) => (
+        <React.Fragment>
+          <img
+            src={value.image}
+            alt={value.imageAlt}
+            className="design-top-img"
+          />
+          {value.text.map((textvalue) => (
+            <p>{textvalue}</p>
+          ))}
+          {value.link && <a href={value.link}>{value.link}</a>}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+function createDesignEntrySide(designEntry) {
+  return (
+    <div className="design-side-grid-container">
+      {designEntry.parts.map((value) => (
+        <React.Fragment>
+          <div className="design-side-left">
+            {value.text.map((textvalue) => (
+              <p>{textvalue}</p>
+            ))}
+            {value.link && <a href={value.link}>{value.link}</a>}
+          </div>
+          <div className="design-side-right">
+            <img
+              src={value.image}
+              alt={value.imageAlt}
+              className="design-side-img"
+            />
+          </div>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
--- src/services/fakeDesignService.js
+++ src/services/fakeDesignService.js
@@ -1,15 +1,37 @@
 import robopopMain from "../img/robopop/Robo-Pop-hands-on.png";
-import rpQuestionaire from "../img/robopop/rp-questionaire-square-png";
+import rpQuestionaire from "../img/robopop/rp-questionaire-square.png";
 import rpUniverseum from "../img/robopop/rp-universeum.png";
 import rpExhibit from "../img/robopop/rp-exhibit.png";
 import rpProgramming from "../img/robopop/rp-programming.png";
 import rpFinished from "../img/robopop/rp-finished.png";
 
+import tbTrainbuddy from "../img/trainbuddy/tb-trainbuddy.png";
+import tbWhiteBoard from "../img/trainbuddy/tb-whiteboard.JPG";
+import tbDesignProcess from "../img/trainbuddy/tb-designprocess.JPG";
+import tbSurvey from "../img/trainbuddy/tb-survey.png";
+import tbTrainStation from "../img/trainbuddy/tb-trainstation.png";
+import tbLogo from "../img/trainbuddy/trainbuddy-logo.png";
+
+import accessLogo from "../img/access/accessLogoTrans.png";
+import accessCompAnalys from "../img/access/competitorAnalysisWithBrands.png";
+import accessBrandPlatform from "../img/access/brandplatform.png";
+import accessPersonas from "../img/access/ac-personas.png";
+import accessCustomerJourney from "../img/access/ac-customerjourney-trans.png";
+import accessApp from "../img/access/AccessApp.png";
+
+import pcConceptArt from "../img/privacycoin/pc-conceptart.jpg";
+import pcWhiteboard from "../img/privacycoin/pc-whiteboard.JPG";
+import pcSketches from "../img/privacycoin/pc-sketches.png";
+import pcLowFi from "../img/privacycoin/pc-lowfiprototype.jpg";
+import pcHiFi from "../img/privacycoin/pc-hifiprototype.JPG";
+import pcComputer from "../img/privacycoin/pc-computer.png";
+import pcEvaluation from "../img/privacycoin/pc-evaluation.png";
+import pcPoster from "../img/privacycoin/pc-poster.jpg";
+
 const DesignPortfolio = [
   {
     _id: 10,
     title: "Robo Pop",
-    layout: "side", //top, side, double
     date: "August to September 2018",
     problem:
       "Design installation aimed at making girls (age 12-16) interested in sensory technology and Internet of things.",
@@ -27,118 +49,315 @@ const DesignPortfolio = [
       {
         _id: 11,
         title: "User Research",
-        layout: "double",
-        image1: rpQuestionaire,
-        image1alt: "A screenshot of a questionnaire used during the project",
-        image2: rpUniverseum,
-        image2alt: "A photo of the science centre where we did the project",
-        text: [
-          "To get a better perspective on our main target group, teenage girls, we did go to school and did short interviews in the form of a questionnaire. It included questions about knowledge of Internet of Things, as well as habits around household items.",
-          "A secondary, but very important, user group that we had to consider were young children as our design needed to be durable enough to survive interaction with them. As such we followed a school class to the science centre and did observational studies such as the shadowing protocol.",
+        layout: "double", //top, side, double
+        parts: [
+          {
+            image: rpQuestionaire,
+            imagealt: "A screenshot of a questionnaire used during the project",
+            text: [
+              "To get a better perspective on our main target group, teenage girls, we did go to school and did short interviews in the form of a questionnaire. It included questions about knowledge of Internet of Things, as well as habits around household items.",
+            ],
+          },
+          {
+            image: rpUniverseum,
+            imagealt: "A photo of the science centre where we did the project",
+
+            text: [
+              "A secondary, but very important, user group that we had to consider were young children as our design needed to be durable enough to survive interaction with them. As such we followed a school class to the science centre and did observational studies such as the shadowing protocol.",
+            ],
+          },
         ],
       },
       {
         _id: 12,
         title: "Prototyping",
         layout: "double",
-        image1: rpExhibit,
-        image1alt: "A photo of the Robo Pop exhibition",
-        image2: rpProgramming,
-        image2alt: "A photo of someone programming a drum for Robo Pop",
-        text: [
-          "During this project we constructed real working physical prototypes that included all the functionality that an IoT band needed. Robotic arms, sensors and the ability to send message to a server were things I worked on.",
-          "This meant that we did a lot more coding during this project than I usually do, and as it was a very machine oriented project it meant that I had to learn Arduino during this course.",
+        parts: [
+          {
+            image: rpExhibit,
+            imagealt: "A photo of the Robo Pop exhibition",
+            text: [
+              "During this project we constructed real working physical prototypes that included all the functionality that an IoT band needed. Robotic arms, sensors and the ability to send message to a server were things I worked on.",
+            ],
+          },
+          {
+            image: rpProgramming,
+            imagealt: "A photo of someone programming a drum for Robo Pop",
+            text: [
+              "This meant that we did a lot more coding during this project than I usually do, and as it was a very machine oriented project it meant that I had to learn Arduino during this course.",
+            ],
+          },
         ],
       },
       {
         _id: 13,
         title: "Results",
         layout: "top",
-        image1: rpFinished,
-        image1alt: "A photo of the Robo Pop exhibition",
-        text: [
-          "The prototype we created turned out great. It got lots of attention during the exhibition and was one of the more popular installations. And more information about it can be found at this website:",
-          <a href="http://idxpo.se/2018/robopop/">
-            http://idxpo.se/2018/robopop/
-          </a>,
-          "However, as successful as it was with the visitors of the science centre, we did not succeed to the extent that we wanted to in attracting our main target group. There were very few preteend and teenage girls at the science center during the exhibition and we succeeded a bit too well in attracting our secondary user group in young children.",
-          "All in all I am happy with the project, but it could benefit from another iteration to expand upon the ideas.",
+        parts: [
+          {
+            image: rpFinished,
+            imagealt: "A photo of the Robo Pop exhibition",
+            text: [
+              "The prototype we created turned out great. It got lots of attention during the exhibition and was one of the more popular installations. ",
+              "However, as successful as it was with the visitors of the science centre, we did not succeed to the extent that we wanted to in attracting our main target group. There were very few preteend and teenage girls at the science center during the exhibition and we succeeded a bit too well in attracting our secondary user group in young children.",
+              "All in all I am happy with the project, but it could benefit from another iteration to expand upon the ideas.",
+              "More information about the project can be found at this website:",
+            ],
+            link: "http://idxpo.se/2018/robopop/",
+          },
         ],
       },
     ],
   },
-]; /*
   {
     _id: 20,
-    title: "",
-    layout: "", //top, side, double
-    date: "",
-    problem: "",
-    design: "",
-    role: "",
-    responsibilities: ["", "", ""],
-    image: "",
-    imagealt: "",
-    imagecredit: "",
+    title: "Train Buddy",
+    date: "September to October 2017",
+    problem:
+      "To come up with a design for a user group in a certain situation, in our case “immigrants” and “the train”.",
+    design:
+      "An app that help immigrants by introducing them to helpful natives on the train.",
+    role: "Design Student",
+    responsibilities: [
+      "Ideation",
+      "Creating a Design Process",
+      "User Research",
+    ],
+    image: tbTrainbuddy,
+    imagealt: "Two icons representing a train and a person with suitcases.",
     entires: [
       {
         _id: 21,
-        title: "",
-        layout: "",
-        image1: "",
-        image1alt: "",
-        text: [""],
+        title: "Ideation",
+        layout: "double",
+        parts: [
+          {
+            image: tbWhiteBoard,
+            imagealt: "A picture of a whiteboard",
+            text: [
+              "The project started with a large brainstorming session, which was then augmented a sketch storming technique that incorporated concepts from the 635 brainwriting technique.",
+            ],
+          },
+          {
+            image: tbDesignProcess,
+            imagealt:
+              "A picture of a design process dawn on paper and sticky notes",
+            text: [
+              "An important part of this project was that we were for the first time creating our own design process",
+            ],
+          },
+        ],
+      },
+      {
+        _id: 22,
+        title: "User Studies",
+        layout: "double",
+        parts: [
+          {
+            image: tbSurvey,
+            imagealt: "A picture of a survey",
+            text: [
+              "I together with another student wrote a questionnaire to investigate how train habits differ between immigrants and natives.",
+            ],
+          },
+          {
+            image: tbTrainStation,
+            imagealt: "A picture of a trainstation",
+            text: [
+              "This survey was conducted both on the internet and also done on location at a train station. This taught me how to approach people",
+            ],
+          },
+        ],
+      },
+      {
+        _id: 23,
+        title: "Results",
+        layout: "top",
+        parts: [
+          {
+            image: tbLogo,
+            imagealt: "The logo of the TrainBuddy project",
+            text: [
+              "The Train Buddy project was important to me as it was the first time I got to feel like I was responsible for the project. In earlier projects we had been told what methods to use and when to use them, and this time it was all up to us.",
+              "There are some things that could have been improved though. We had very tight time constraints, and as such we didn’t have time to implement the findings of the user studies into the prototype properly.",
+              "Overall this project taught me a lot.This was the first time I interviewed real people and tried to come up with ideas to solve real problems, rather than working with other students pretending to be users. And I learn a lot of valuable lessons regarding how to structure a design project.",
+            ],
+          },
+        ],
       },
     ],
   },
   {
     _id: 30,
-    title: "",
-    layout: "", //top, side, double
-    date: "",
-    problem: "",
-    design: "",
-    role: "",
-    responsibilities: ["", "", ""],
-    image: "",
-    imagealt: "",
-    imagecredit: "",
+    title: "Access",
+    date: "August to October 2018",
+    problem:
+      "A company that made paper towel and related products wanted rebrand themselves as more inclusive and have that reflected in their products",
+    design:
+      "An app that helped facility managers and installers with find and placing the correct products to increase inclusivity.",
+    role: "Design Student",
+    responsibilities: ["Brand Redesign", "User Studies", "Prototyping"],
+    image: accessLogo,
+    imagealt: "The logo for the redesigned version of Access",
     entires: [
       {
         _id: 31,
-        title: "",
-        layout: "",
-        image1: "",
-        image1alt: "",
-        text: [""],
+        title: "Brand Redesign",
+        layout: "double",
+        parts: [
+          {
+            image: accessCompAnalys,
+            imagealt: "An image displaying a tool to compare brands",
+            text: [
+              "As this project was not just about designing a product, but to design an entire brand we were introduced to tools to help with that. For example we conducted competitor brand analysis together with moodboards to understand what was important aspect to our brands and its competitors.",
+            ],
+          },
+          {
+            image: accessBrandPlatform,
+            imagealt: "An image displaying the brand platform tool",
+            text: [
+              "At the end of the project we had created a new Brand Platform for Access, and had written a set of guidelines for how to design for this new brand, that we then had to follow as we our primary focus shifted to create a product for the new brand.",
+            ],
+          },
+        ],
+      },
+      {
+        _id: 32,
+        title: "User Studies",
+        layout: "double",
+        parts: [
+          {
+            image: accessPersonas,
+            imagealt:
+              "An image displaying pictures used for personas during the Access project",
+            text: [
+              "In order to understand what it was that made the current version of products appear less inclusive than desired we interviewed people who had come in contact with existing products of the brand and constructed simple personas that highlighted issues with inclusivity.",
+            ],
+          },
+          {
+            image: accessCustomerJourney,
+            imagealt: "A screenshot of a customer journey.",
+            text: [
+              "These personas were then used to construct Customer Journeys, where we simulated a normal interaction with the existing products and tried to estimate the emotional experience at each instance of the interaction.",
+            ],
+          },
+        ],
+      },
+      {
+        _id: 33,
+        title: "Results",
+        layout: "side",
+        parts: [
+          {
+            image: accessApp,
+            imagealt:
+              "A high fidelity image of the starting screen of the Access app made in illustrator",
+            text: [
+              "Access was one of the last projects I worked on before I started with my master’s thesis and as it was a project I did for a real company it made the project feel very real.",
+              "The final product we presented was an app that had been sketched out in Illustrator. As a group we did put more of our focus into the brand redesign than the product design, and I honestly think the product suffered from that. Had I been able to redo the project I would have used Figma or XD to create a higher fidelity prototype with some actual functionality. ",
+              "I am happy with the rebranding. I picked up some valuable lessons on how to work with a brand, and an understanding on how brands work.",
+            ],
+          },
+        ],
       },
     ],
   },
   {
     _id: 40,
-    title: "",
-    layout: "", //top, side, double
-    date: "",
-    problem: "",
-    design: "",
-    role: "",
-    responsibilities: ["", "", ""],
-    image: "",
-    imagealt: "",
-    imagecredit: "",
+    title: "Privacy Coin",
+    date: "January to March 2018",
+    problem: "Design a tangible interface on the theme of internet safety.",
+    design:
+      "An installation that aims to make people think about how they are paying with their personal data by making that transaction tangible.",
+    role: "Design Student",
+    responsibilities: ["Ideation", "Physical Prototyping", "Evaluation"],
+    image: pcConceptArt,
+    imagealt: "Concept art for the finalized version of privacy coin.",
+    imagecredit: "Image by Gregory Axton",
     entires: [
       {
         _id: 41,
-        title: "",
-        layout: "",
-        image1: "",
-        image1alt: "",
-        text: [""],
+        title: "Ideation",
+        layout: "double",
+        parts: [
+          {
+            image: pcWhiteboard,
+            imagealt:
+              "A photo of one of the whiteboards used during this project.",
+            text: [
+              "This project used different brainstorming techniques at many points of process. Not only did we use it to come up with the initial idea, but we went back kept using them to flesh out different details.",
+            ],
+          },
+          {
+            image: pcSketches,
+            imagealt:
+              "A photo of some of the sketches made during the project.",
+            text: [
+              "We also used a lot of sketches during this project. Many things were very conceptual in their natures, and sketches helped us realize them into tangible prototypes.",
+            ],
+          },
+        ],
+      },
+      {
+        _id: 42,
+        title: "Prototyping",
+        layout: "double",
+        parts: [
+          {
+            image: pcLowFi,
+            imagealt: "Three low-fi prototypes side by side.",
+            text: [
+              "Physical prototyping was a very important aspect of this project and we used different levels of fidelity at different points of the project. Starting really Low-fi, with paper and junk we found in the studio.",
+            ],
+          },
+          {
+            image: pcHiFi,
+            imagealt: "The hi-fi prototype displayed in a classroom.",
+            text: [
+              "In the end we had a working Hi-fi prototype with all of the functionality we wanted, apart from perhaps needing a better paint job. This could then be used for our evaluation sessions.",
+            ],
+          },
+        ],
+      },
+      {
+        _id: 43,
+        title: "Evaluation",
+        layout: "double",
+        parts: [
+          {
+            image: pcComputer,
+            imagealt: "A computer used simulate the collections of data.",
+            text: [
+              "Once the prototype was complete we had an test session where the part of the experience that we couldn’t create with the prototype was simulated with the Wizard of Oz protocol.",
+            ],
+          },
+          {
+            image: pcEvaluation,
+            imagealt: "The evaluation form used during this project.",
+            text: [
+              "We then evaluated the test session using the Semantic Differential Method, this had the benefit from say a Likert Scale by the presupposing that a higher number is better than a lower number.",
+            ],
+          },
+        ],
+      },
+      {
+        _id: 44,
+        title: "Results",
+        layout: "side",
+        parts: [
+          {
+            image: pcPoster,
+            imagealt: "A poster used for Privacy Coin",
+            text: [
+              "This was an interesting project as it were tasked with creating a tangible interface for something that is generally considered a digital problem. Also we have really good cohesion within the team, and I think that a lot of it stemmed from the creative environment that we had created during the project, which is something I try to bring with me into future projects.",
+              "The idea we came up with was also very different from what the other groups did during the project, most of them doing authentication devices and tangible password managers. Though it may be a bit too artsy for a corporate setting.",
+            ],
+          },
+        ],
       },
     ],
   },
 ];
-*/
 
 export function getDesignPortfolio() {
   return DesignPortfolio;
