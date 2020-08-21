77c9d7d714f516305e44aed23a1498c3402e6b14
bbjorn <bbjorn@student.chalmers.se>
2020-08-21 07:34:12 +0200

    Added functionality to not split view for narrowscreens.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { getAboutMe } from "./../services/fakeAboutMeService";
 import "./../css/aboutme.css";
 
 function AboutMe(props) {
+  var { narrowScreen } = props;
   const aboutMe = getAboutMe();
   return (
     <div className="mainstyle-inner">
       <div>
         <h2 className="h2-box">{aboutMe[0].title}</h2>
-        {createAboutMeElement(aboutMe[0])}
+        {createAboutMeElement(aboutMe[0], narrowScreen)}
       </div>
+      {aboutMeSplit(narrowScreen, aboutMe)}
+    </div>
+  );
+}
+
+function aboutMeSplit(narrowScreen, aboutMe) {
+  if (narrowScreen)
+    return (
+      <Fragment>
+        <div>
+          <h2 className="h2-box">{aboutMe[1].title}</h2>
+          {createAboutMeElement(aboutMe[1])}
+        </div>
+        <div>
+          <h2 className="h2-box">{aboutMe[2].title}</h2>
+          {createAboutMeElement(aboutMe[2])}
+        </div>
+      </Fragment>
+    );
+  return (
+    <Fragment>
       <div className="aboutme-title-grid-container">
         <div className="aboutme-title-left">
           <h2 className="h2-box">{aboutMe[1].title}</h2>
@@ -22,23 +44,26 @@ function AboutMe(props) {
         <div className="aboutme-left">{createAboutMeElement(aboutMe[1])}</div>
         <div className="aboutme-right">{createAboutMeElement(aboutMe[2])}</div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
-/*
-
-<div class="aboutme-title-left">
-          <h2 className="h2-box">{aboutMe[1].title}</h2>
-        </div>
-        <div class="aboutme-title-right">
-          <h2 className="h2-box">{aboutMe[2].title}</h2>
-        </div>
- 
-*/
-
-function createAboutMeElement(aboutMeElem) {
+function createAboutMeElement(aboutMeElem, narrowScreen) {
   if (aboutMeElem.image) {
+    if (narrowScreen) {
+      return (
+        <div>
+          <img
+            src={aboutMeElem.image}
+            alt={aboutMeElem.imageAlt}
+            className="aboutMe-img"
+          />
+          {aboutMeElem.text.map((value, index) => {
+            return <p key={index}>{value}</p>;
+          })}
+        </div>
+      );
+    }
     return (
       <div className="aboutme-img-grid-container">
         <div className="aboutme-img-left">
