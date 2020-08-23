c94cd70a6d5aea56a45dac6f3981dba57c53c741
bbjorn <bbjorn@student.chalmers.se>
2020-08-23 12:51:32 +0200

    Split navbar for Portofolio into two bars.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,58 +3,70 @@ import { NavLink, Link, withRouter } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
 import { getDesignPortfolio } from "./../services/fakeDesignService";
 import "../css/navbar.css";
+import { Fragment } from "react";
 
 function Navbar(props) {
   return (
-    <nav className="navbar">
-      <ul className="navbar-ul">
-        <li className="navbar-li">
-          <NavLink className="nav-item nav-link" exact to="/portfolio/">
-            Start
-          </NavLink>
-        </li>
-        <li className="navbar-li">
-          <NavLink className="nav-item nav-link" exact to="/portfolio/aboutme">
-            About Me
-          </NavLink>
-        </li>
-        <li className="navbar-li">
-          <NavLink className="nav-item nav-link" exact to="/portfolio/design">
-            Design Portfolio
-          </NavLink>
-        </li>
-        {window.location.pathname === "/portfolio/design" &&
-          createDesignNavbar()}
-        <li className="navbar-li">
-          <NavLink className="nav-item nav-link" exact to="/portfolio/code">
-            Code Portfolio
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className="navbar-sticky">
+      <nav className="navbar">
+        <ul className="navbar-ul">
+          <li className="navbar-li">
+            <NavLink className="nav-item nav-link" exact to="/portfolio/">
+              Start
+            </NavLink>
+          </li>
+          <li className="navbar-li">
+            <NavLink
+              className="nav-item nav-link"
+              exact
+              to="/portfolio/aboutme"
+            >
+              About Me
+            </NavLink>
+          </li>
+          <li className="navbar-li">
+            <NavLink className="nav-item nav-link" exact to="/portfolio/design">
+              Design Portfolio
+            </NavLink>
+          </li>
+          <li className="navbar-li">
+            <NavLink className="nav-item nav-link" exact to="/portfolio/code">
+              Code Portfolio
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      {window.location.pathname === "/portfolio/design" && createDesignNavbar()}
+    </div>
   );
 }
 
 /* Shows navbar elements for each project in the design portfolio, only when veiwing the design portfolio. */
 function createDesignNavbar() {
   const designPortfolio = getDesignPortfolio();
-  return designPortfolio.map((value, index) => (
-    <li className="navbar-li" key={index * -1}>
-      <NavHashLink
-        className="nav-item nav-link"
-        exact
-        to={{
-          pathname: "/portfolio/design",
-          hash: `#${value.titleNoSpace}`,
-        }}
-        smooth={true}
-        duration={1000}
-        key={index}
-      >
-        {value.title}
-      </NavHashLink>
-    </li>
-  ));
+  return (
+    <nav className="navbar">
+      <ul className="navbar-ul">
+        {designPortfolio.map((value, index) => (
+          <li className="navbar-li" key={index * -1}>
+            <NavHashLink
+              className="nav-item nav-link"
+              exact
+              to={{
+                pathname: "/portfolio/design",
+                hash: `#${value.titleNoSpace}`,
+              }}
+              smooth={true}
+              duration={1000}
+              key={index}
+            >
+              {value.title}
+            </NavHashLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default withRouter(Navbar);
