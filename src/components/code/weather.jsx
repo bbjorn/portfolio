19ba9b65a20e5dc5ecf688fce98e5b5da0739a77
bbjorn <bbjorn@student.chalmers.se>
2020-08-23 18:38:20 +0200

    Got an API for getting the weather.

--- src/components/code/weather.jsx
+++ src/components/code/weather.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function weather(props) {
-  useEffect(() => {}, []);
+  // Component did mount
+  /*
+    useEffect(() => {
+        // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={YOUR API KEY}
+    }, 
+  []);
+  */
 
   return <div></div>;
 }
