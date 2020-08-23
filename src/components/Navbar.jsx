8ec4bdfa582c1177ace646de7aa6b6e4a3f53123
bbjorn <bbjorn@student.chalmers.se>
2020-08-23 17:05:30 +0200

    Fixed issues with navbar items overlapping.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,50 @@
 import React from "react";
-import { NavLink, Link, withRouter } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
 import { getDesignPortfolio } from "./../services/fakeDesignService";
 import "../css/navbar.css";
-import { Fragment } from "react";
 
 function Navbar(props) {
+  const designPortfolio = getDesignPortfolio();
   return (
     <div className="navbar-sticky">
       <nav className="navbar">
-        <ul className="navbar-ul">
-          <li className="navbar-li">
-            <NavLink className="nav-item nav-link" exact to="/portfolio/">
-              Start
-            </NavLink>
-          </li>
-          <li className="navbar-li">
-            <NavLink
-              className="nav-item nav-link"
-              exact
-              to="/portfolio/aboutme"
-            >
-              About Me
-            </NavLink>
-          </li>
-          <li className="navbar-li">
-            <NavLink className="nav-item nav-link" exact to="/portfolio/design">
-              Design Portfolio
-            </NavLink>
-          </li>
-          <li className="navbar-li">
-            <NavLink className="nav-item nav-link" exact to="/portfolio/code">
-              Code Portfolio
-            </NavLink>
-          </li>
-        </ul>
+        <NavLink className="nav-item" exact to="/portfolio/">
+          Start
+        </NavLink>
+        <NavLink className="nav-item" exact to="/portfolio/aboutme">
+          About Me
+        </NavLink>
+        <NavLink className="nav-item" exact to="/portfolio/design">
+          Design Portfolio
+        </NavLink>
+        {window.location.pathname === "/portfolio/design" &&
+          createDesignNavbar(designPortfolio)}
+        <NavLink className="nav-item" exact to="/portfolio/code">
+          Code Portfolio
+        </NavLink>
       </nav>
-      {window.location.pathname === "/portfolio/design" && createDesignNavbar()}
     </div>
   );
 }
 
-/* Shows navbar elements for each project in the design portfolio, only when veiwing the design portfolio. */
-function createDesignNavbar() {
-  const designPortfolio = getDesignPortfolio();
-  return (
-    <nav className="navbar">
-      <ul className="navbar-ul">
-        {designPortfolio.map((value, index) => (
-          <li className="navbar-li" key={index * -1}>
-            <NavHashLink
-              className="nav-item nav-link"
-              exact
-              to={{
-                pathname: "/portfolio/design",
-                hash: `#${value.titleNoSpace}`,
-              }}
-              smooth={true}
-              duration={1000}
-              key={index}
-            >
-              {value.title}
-            </NavHashLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+/* Creates navlinks for all projects inside a portfolio.  */
+function createDesignNavbar(portfolio) {
+  return portfolio.map((value, index) => (
+    <NavHashLink
+      className="nav-item"
+      exact
+      to={{
+        pathname: "/portfolio/design",
+        hash: `#${value.titleNoSpace}`,
+      }}
+      smooth={true}
+      duration={1000}
+      key={index}
+    >
+      {value.title}
+    </NavHashLink>
+  ));
 }
 
 export default withRouter(Navbar);
